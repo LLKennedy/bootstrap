cecb5e409583361dcf36d436ae2de5de345d95b1
Luke Kennedy <luke.l.kennedy@gmail.com>
2019-12-01 10:27:00 +1100

    Extract tools to a single folder

--- HashiCorp/Get-HashiStack.ps1
+++ HashiCorp/Get-HashiStack.ps1
@@ -19,14 +19,15 @@ Function Get-HashiCorpBinary {
 
 		[parameter(Mandatory=$false)]
 		[String]
-		$OutFile
+		$OutDirectory
 	)
 	Write-Output "Downloading ${Product}..."
 	$hashData = $(Invoke-WebRequest -Uri "https://releases.hashicorp.com/${Product}/${Version}/${Product}_${Version}_SHA256SUMS" -ErrorAction Stop).Content
-	# TODO: Get signature file for checksum file and verify before bothering with the main zip download
-	$mainFileResponse = $(Invoke-WebRequest -Uri "https://releases.hashicorp.com/${Product}/${Version}/${Product}_${Version}_${OSWithArch}.zip" -ErrorAction Stop)
+	# We're just going to trust PKI, if they control hashicorp.com we'll trust the checksum file without verifying the sig file
+	$fullProductName = "${Product}_${Version}_${OSWithArch}"
+	Invoke-WebRequest -Uri "https://releases.hashicorp.com/${Product}/${Version}/${fullProductName}.zip" -ErrorAction Stop -OutFile "${fullProductName}.zip"
 	$matchFound = $false
-	$fileHash = $(Get-FileHash -InputStream $mainFileResponse.RawContentStream -Algorithm SHA256).Hash
+	$fileHash = $(Get-FileHash -Algorithm SHA256 -Path "${fullProductName}.zip").Hash
 	ForEach ($line in $($hashData -split "\n")) { # this isn't os-specific, hashicorp always uses simple \n to separate
 		$parts = $line -split "  "
 		if ($parts[1] -eq "${Product}_${Version}_${OSWithArch}.zip" -and $parts[0] -eq $fileHash) {
@@ -38,22 +39,28 @@ Function Get-HashiCorpBinary {
 		throw "Downloaded zip for ${Product} at version ${Version} was corrupt or invalid"
 	}
 
-	# TODO: Unzip to specified file location
+	$(Expand-Archive -Path "${fullProductName}.zip" -DestinationPath $fullProductName)
 
-	Write-Progress -Activity "Saving ${Product} to file system"
-	Set-Content -Path $OutFile -Value $mainFileResponse.Content -AsByteStream
-	Write-Progress -Activity "Saving ${Product} to file system" -Completed $true
+	$file = $(Get-ChildItem -Path $fullProductName)
+	Move-Item -Path "$fullProductName/$($file.PSChildName)" -Destination "$OutDirectory/$($file.PSChildName)"
+	Remove-Item $fullProductName # Remove the unzipped folder now that it's empty
 	Write-Output "${Product} downloaded!"
 }
 
 Write-Output "Downloading HashiCorp stack..."
 
+$tools = "./HashiCorp Tools"
+
+if (!$(Test-Path $tools)) {
+	New-Item -Path $tools -ItemType "directory"
+}
+
 # Consul
 $ConsulVersion = "1.6.2"
 $OSWithArch = "windows_amd64"
-Get-HashiCorpBinary -Product "consul" -Version $ConsulVersion -OSWithArch ${OSWithArch} -OutFile "consul.zip" # TODO: Change to .exe when unzipping is done
+Get-HashiCorpBinary -Product "consul" -Version $ConsulVersion -OSWithArch ${OSWithArch} -OutDirectory $tools
 
 # Vault
 $ConsulVersion = "1.3.0"
 $OSWithArch = "windows_amd64"
-Get-HashiCorpBinary -Product "vault" -Version $ConsulVersion -OSWithArch ${OSWithArch} -OutFile "vault.zip" # TODO: Change to .exe when unzipping is done
+Get-HashiCorpBinary -Product "vault" -Version $ConsulVersion -OSWithArch ${OSWithArch} -OutDirectory $tools
