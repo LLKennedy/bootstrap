bbdabdce4e1f1319dd1235485b5f76663ffbde17
Luke Kennedy <luke.l.kennedy@gmail.com>
2019-11-30 22:45:28 +1100

    Create simple hello world script

---
+++ New-ServiceEnvironment.ps1
@@ -0,0 +1,5 @@
+<#
+    .Description
+    New-ServiceEnvironment sets up a full microservice environment from scratch, with no pre-requisites other than powershell core
+#>
+Write-Output "hello world"
